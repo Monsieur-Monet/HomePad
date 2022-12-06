6e6840265df87ae5158caf09915d1181c1bc590a
timo.scheer <timo.scheer@bytediver.de>
2022-12-06 12:41:20 +0100

    some changes

--- scripts/index.js
+++ scripts/index.js
@@ -3,4 +3,30 @@ import { awaitDOM } from './helpers/sleep.js';
 
 awaitDOM().then(_ => {
   BackgroundComponent();
+  Greeter();
 });
+
+function Greeter() {
+  const greetValue = Math.floor(Math.random() * 5) + 1;
+  if (greetValue === 1) {
+    let paragraph = document.getElementById('greet');
+    let greet = document.createTextNode('Hey!');
+    paragraph.appendChild(greet);
+  } else if (greetValue === 2) {
+    let paragraph = document.getElementById('greet');
+    let greet = document.createTextNode('Hello!');
+    paragraph.appendChild(greet);
+  } else if (greetValue === 3) {
+    let paragraph = document.getElementById('greet');
+    let greet = document.createTextNode('Good Day!');
+    paragraph.appendChild(greet);
+  } else if (greetValue === 4) {
+    let paragraph = document.getElementById('greet');
+    let greet = document.createTextNode('Hello There!');
+    paragraph.appendChild(greet);
+  } else if (greetValue === 5) {
+    let paragraph = document.getElementById('greet');
+    let greet = document.createTextNode('Hi There!');
+    paragraph.appendChild(greet);
+  }
+}
